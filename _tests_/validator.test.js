5a998f6deeb7b00852c64f2b1d7ba97bb51189a7
dorjepadma <dorjepadma@gmail.com>
2020-03-10 16:18:19 -0700

    validator writter

---
+++ _tests_/validator.test.js
@@ -0,0 +1,12 @@
+const Validator = required('./Validator')
+describe('Validator,'() => {
+  it('has a field and configuration property')
+  const nameValidator = new Validator('name', {
+    type: String.
+    required: true
+  });
+  expect(nameValidator.field.toEqual('name');
+  expect(name.Validator.configuration);
+  
+  ))
+})
