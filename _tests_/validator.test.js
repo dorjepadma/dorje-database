1d274cef570ed4a59b4c56cfb90bf8aa56aef308
dorjepadma <dorjepadma@gmail.com>
2020-03-10 17:21:47 -0700

    validation test done

--- _tests_/validator.test.js
+++ _tests_/validator.test.js
@@ -1,12 +1,55 @@
-const Validator = required('./Validator')
-describe('Validator,'() => {
-  it('has a field and configuration property')
-  const nameValidator = new Validator('name', {
-    type: String.
-    required: true
-  });
-  expect(nameValidator.field.toEqual('name');
-  expect(name.Validator.configuration);
+const Validator = require('../lib/Validator');
+
+describe('Validator', () => {
+    let nameValidator;
+
+    beforeEach(() => {
+        nameValidator = new Validator('name', {
+            type: String,
+            required: true
+        });
+    });
+    it('has a field and configuration property', () => {
+        expect(nameValidator.field).toEqual('name');
+        expect(nameValidator.configuration).toEqual({
+            type: String,
+            required: true
+        });
+    });
+    it('can validate an object with the property type', () => {
+        const cat = {
+            name: 'trixie',
+            age: 3,
+            weight: '12 lbs'
+        };
+
+        expect(nameValidator.validate(cat)).toEqual('trixie');
+    });
+    it('can validate an object with the wrong type but castable', () => {
+        const cat = {
+            name: 9876,
+            age: 5,
+            weight: '12 lbs'
+        };
+        expect(nameValidator.validate(cat)).toEqual('9876');
+    });
+
+    it('throws an error if it validates an object with the wrong type and not castable', () => {
+        const cat = {
+            name: {},
+            age: 3,
+            weight: '200 lbs'
+        };
+
+        expect(() => nameValidator.validate(cat)).toThrowError('Cannot cast >>[object Object]<< to String');
+    });
+
+    it('throws an error when validating an object with a missing field', () => {
+        const cat = {
+            age: 4,
+            weight: '8 lbs'
+        };
+        expect(() => nameValidator.validate(cat)).toThrowError('Missing required field >>name<<');
+    });
+});
   
-  ))
-})
