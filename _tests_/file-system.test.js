371746baa73857096822b8568029e4205e7e0156
dorjepadma <dorjepadma@gmail.com>
2020-03-11 18:03:26 -0700

    three testes in filesystem written

---
+++ _tests_/file-system.test.js
@@ -0,0 +1,47 @@
+const fs = require('fs').promises;
+const { 
+    mkdirp,
+    writeJSON,
+    readJSON
+} = require('../lib/file-system');
+
+jest.mock('fs', () => ({
+    promises: {
+        mkdir: jest.fn(() => Promise.resolve()),
+        writeFile: jest.fn(() => Promise.resolve()),
+        readFile: jest.fn(() => Promise.resolve('{"name":"trixie"}'))
+    }
+}));
+
+describe ('files system functions', () => {
+    it('makes a directory and all parent directories', () => {
+        return mkdirp('./dewachen/amitabha/lotus/here')
+            .then(() => {
+                expect(fs.mkdir)
+                    .toHaveBeenCalledWith('./dewachen/amitabha/lotus/here', { recursive: true});
+            });
+    });
+    it('writes an object to a file', () => {
+        const cat = {
+            name: 'trixie',
+            age: 5,
+            weight: '11 lbs'
+        };
+
+        return writeJSON('./test.json, cat')
+            .then(() => {
+                expect(fs.writeFile)
+                    .toHaveBeenCalledWith('./test.json', JSON.stringify(cat));
+            });
+    });
+    it('can read an object from a file', () => {
+        return readJSON('./test.json')
+            .then(data => {
+                expect(fs.readFile)
+                    .toHaveBeenCalledWith('./test.json');
+                expect(data).toEqual({
+                    name:'trixie'
+                });
+            });
+    });
+});
