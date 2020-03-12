fa2e0c703c462d527bad5d666989807045536fb3
dorjepadma <dorjepadma@gmail.com>
2020-03-11 21:31:38 -0700

    first three test working

--- _tests_/file-system.test.js
+++ _tests_/file-system.test.js
@@ -2,7 +2,8 @@ const fs = require('fs').promises;
 const { 
     mkdirp,
     writeJSON,
-    readJSON
+    readJSON,
+   
 } = require('../lib/file-system');
 
 jest.mock('fs', () => ({
@@ -18,9 +19,10 @@ describe ('files system functions', () => {
         return mkdirp('./dewachen/amitabha/lotus/here')
             .then(() => {
                 expect(fs.mkdir)
-                    .toHaveBeenCalledWith('./dewachen/amitabha/lotus/here', { recursive: true});
+                    .toHaveBeenCalledWith('./dewachen/amitabha/lotus/here', { recursive: true });
             });
     });
+
     it('writes an object to a file', () => {
         const cat = {
             name: 'trixie',
@@ -28,10 +30,14 @@ describe ('files system functions', () => {
             weight: '11 lbs'
         };
 
-        return writeJSON('./test.json, cat')
+        return writeJSON('./test.json', cat)
             .then(() => {
+        // check that write file is called with the correct arguments
+        // './test.json', and JSON.stringify(cat)
                 expect(fs.writeFile)
                     .toHaveBeenCalledWith('./test.json', JSON.stringify(cat));
+        // read the file
+        // make sure the file has the right stuff in it
             });
     });
     it('can read an object from a file', () => {
