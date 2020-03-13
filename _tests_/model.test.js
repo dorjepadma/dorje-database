1185c9d03ec9ba15d6cf31a74e7f8153f1cc6cce
dorjepadma <dorjepadma@gmail.com>
2020-03-13 06:56:00 -0700

    test passing on model 1

--- _tests_/model.test.js
+++ _tests_/model.test.js
@@ -1,36 +1,38 @@
-const Schema = require('./Schema');
-const Model = require('./Model');
-const Cat = new Model('Cat', schema);
+const Schema = require('../lib/Schema');
+const Model = require('../lib/model');
 
-const schema = new Schema({
-name: {
-  type: String,
-  required: true
-},
-age: {
-  type: Number, 
-  required: true
-},
-weight: {
-  type: String
-}
-});
 
 describe('Model class', () => {
-  it('creates a new document', () => {
+    it('creates a new document', () => {
+        const schema = new Schema({
+            name: {
+                type: String,
+                required: true
+            },
+            age: {
+                type: Number, 
+                required: true
+            },
+            weight: {
+                type: String
+            }
+        });
+        
+        const Cat = new Model('Cat', schema);
 
-  return Cat
-  .create({
-    name: 'trixie',
-        age: 5,
-        weight: '11 lbs'
-  })
-  .then(cat => {
-    expect(cat).toEqual({
-      _id: expect.any(String),
-      name: 'trixie',
-      age: 5,
-      weight: '11 lbs'
+        return Cat
+            .create({
+                name: 'trixie',
+                age: 5,
+                weight: '11 lbs'
+            })
+            .then(cat => {
+                expect(cat).toEqual({
+                    _id: expect.any(String),
+                    name: 'trixie',
+                    age: 5,
+                    weight: '11 lbs'
+                });
+            });
     });
-  });
 });
